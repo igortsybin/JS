1c82be2911596064c04a7ca31ccd02cb1910b304
igorts <igor.ts.job@gmail.com>
2017-06-02 11:43:42 +0300

    Create Dir, File, Node, Stats JS files & Edit test

--- src/Hexlet/FileSystem.Project/__tests__/HexletFs.test.js
+++ src/Hexlet/FileSystem.Project/__tests__/HexletFs.test.js
@@ -1,4 +1,5 @@
-const HexletFs = require('../HexletFs');
+import HexletFs from '../HexletFs';
+
 
 describe('FS', () => {
   let files;
@@ -6,49 +7,31 @@ describe('FS', () => {
   beforeEach(() => {
     files = new HexletFs();
   });
-  it('#isDirectory', () => {
-    expect(!files.isDirectory('/etc')).toBe(true);
-  });
 
   it('#mkdirSync', () => {
-
-    expect(!files.isDirectory('/etc')).toBe(true);
     files.mkdirSync('/etc');
-    
-    expect(files.isDirectory('/etc')).toBe(true);
-    // files.mkdirSync('/etc'); // extra
+    expect(files.statSync('/etc').isDirectory()).toBeTruthy();
+
     files.mkdirSync('/etc/nginx');
-    // console.log(files);
-    expect(files.isDirectory('/etc/nginx')).toBe(true);
+    expect(files.statSync('/etc/nginx').isDirectory()).toBeTruthy();
   });
 
   it('#mkdirSync2', () => {
-
-    expect(!files.isDirectory('/var//')).toBe(true);
-
     files.mkdirSync('/var/');
-    expect(files.isDirectory('/var////')).toBe(true);
-    expect(files.isDirectory('/var')).toBe(true);
+    expect(files.statSync('/var////').isDirectory()).toBeTruthy();
+    expect(files.statSync('/var').isDirectory()).toBeTruthy();
 
     files.mkdirSync('/var//log//////');
-    expect(files.isDirectory('/var/log')).toBe(true);
-    expect(files.isDirectory('/var///log')).toBe(true);
+    expect(files.statSync('/var/log').isDirectory()).toBeTruthy();
+    expect(files.statSync('/var///log').isDirectory()).toBeTruthy();
   });
 
-  it('#isFile', () => {
-
-    expect(!files.isFile('/file.txt')).toBe(true);
-  });
-  
   it('#touchSync', () => {
-
-    expect(!files.isFile('/file.txt')).toBe(true);
-
     files.touchSync('/file.txt');
-    expect(files.isFile('/file.txt')).toBe(true);
+    expect(files.statSync('/file.txt').isFile()).toBeTruthy();
 
     files.mkdirSync('/etc');
     files.touchSync('/etc/bashrc');
-    expect(files.isFile('/etc/bashrc')).toBe(true);
+    expect(files.statSync('/etc/bashrc').isFile()).toBeTruthy();
   });
 });
