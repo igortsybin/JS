ffc90a87d89beef008f6c16362c10f14158c9a7c
Igor Tsybin <igor.tsybin@edc-electronics.ru>
2017-06-23 16:27:30 +0300

    add callback

--- src/Hexlet/Async.Project/diff.js
+++ src/Hexlet/Async.Project/diff.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
-console.log('yes');
+
 const compare = (data1, data2) => {
   const lines1 = data1.split('\n').slice(0, -1);
   const lines2 = data2.split('\n').slice(0, -1);
@@ -8,12 +8,11 @@ const compare = (data1, data2) => {
   
   // END
 };
+const editPath = path => 'src/Hexlet/Async.Project/'.concat(path);
 
 // BEGIN (write your solution here)
 export default function (part1, part2, callback) {
-  this.readFile('./etc', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
+  const fileEncoding = 'utf8';
+  fs.readFile(editPath(part1), fileEncoding, callback);
 }
 // END
