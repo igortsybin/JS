c8661f0b2c59dc924c8a08bb67fa3c5873612a4d
Igor Tsybin <igor.tsybin@edc-electronics.ru>
2017-07-06 16:20:08 +0300

    try to realize retry

--- src/Hexlet/AsyncCBHell.Project/solution.js
+++ src/Hexlet/AsyncCBHell.Project/solution.js
@@ -1,12 +1,19 @@
-export default (functions, callback) => {
+export default (countErr, ...functions) => {
   if (functions.length === 0) {
-    return callback();
+    return console.log('No functions');
   }
-
+  let count = countErr === 0 ? 5 : countErr;
   const next = ([head, ...rest], previousresult) => {
+    // console.log('Function  = ', head);
     const cb = (err, ...args) => {
-      if (err) {
-        return callback(err, args);
+      if (err && count !== 0) {
+        console.log('previousresult = ', previousresult);
+        // console.log('count = ', count);
+        count -= 1;
+        return head(...previousresult, cb);// head(...previousresult, cb);
+      }
+      if (rest.length === 0) {
+        console.log('No more functions');
       } else {
         next(rest, args);
       }
@@ -15,4 +22,20 @@ export default (functions, callback) => {
   };
   next(functions, []);
 }
+// export default (functions, callback) => {
+//   if (functions.length === 0) {
+//     return callback();
+//   }
 
+//   const next = ([head, ...rest], previousresult) => {
+//     const cb = (err, ...args) => {
+//       if (err) {
+//         return callback(err, args);
+//       } else {
+//         next(rest, args);
+//       }
+//     };
+//     head(...previousresult, cb);
+//   };
+//   next(functions, []);
+// }
